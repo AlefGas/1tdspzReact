51679c4ee83c61152910a926cad059a2ee0ac4de
alefG <lafitealef@gmail.com>
2023-10-30 21:27:18 -0300

    alteração das techs

--- src/pages/techemergentes.jsx
+++ src/pages/techemergentes.jsx
@@ -1,7 +1,7 @@
 export default ()=>{
     return(
         <>
-        <h1>Uma das tecnologias que estão mais conhecidas atualmente é a inteligencia artificial</h1>
+        <h1>Uma das tecnologias que estão mais conhecidas atualmente é a inteligencia artificial, porém tem diversas outras, Juntando todas temos um mundo que está em constante mudanças tecnologicas e que está cada vez mais difícil de acompanhar. </h1>
         <img src="https://img.freepik.com/fotos-premium/dispositivos-de-tecnologia-e-icones-conectados-ao-planeta-terra-digital_117023-449.jpg" alt="Imagem de tecnologias" />
         </>
     )
